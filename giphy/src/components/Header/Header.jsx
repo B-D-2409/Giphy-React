923af809f03ce8f4e29590d1a057d3a30c61a189
Masterclass777 <borislav.2404g@gmail.com>
2025-05-19 14:19:20 +0300

    css changes for sidebar menu

--- giphy/src/components/Header/Header.jsx
+++ giphy/src/components/Header/Header.jsx
@@ -106,6 +106,7 @@ export default function Header() {
                     
                 </nav>
                 </div>
+                
                 {user &&  <SideBar />}
         
         </header>
